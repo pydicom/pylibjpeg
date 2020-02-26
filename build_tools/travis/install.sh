31a9fceb1e3713759a27f79ca0c93e36c7eed5ee
scaramallion <scaramallion@users.noreply.github.com>
2020-02-26 16:52:01 +1100

    Conda try 3

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -34,6 +34,8 @@ elif [[ "$TEST_SUITE" == 'osx' ]]; then
     pip install pydicom
     python -c "import pydicom; print('pydicom version', pydicom.__version__)"
 elif [[ "$TEST_SUITE" == 'conda' ]]; then
+    deactivate
+
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     bash miniconda.sh -b -p $HOME/miniconda
     source "$HOME/miniconda/etc/profile.d/conda.sh"
@@ -43,9 +45,10 @@ elif [[ "$TEST_SUITE" == 'conda' ]]; then
     # Useful for debugging any issues with conda
     conda info -a
     # Replace dep1 dep2 ... with your dependencies
-    conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
+    conda create -q -n test-environment python=$PYTHON_VERSION pip
     conda activate test-environment
-    conda install -c conda-forge pydicom
+    conda install --yes nose pytest pytest-cov setuptools
+    conda install --yes -c conda-forge pydicom
 fi
 
 python --version
