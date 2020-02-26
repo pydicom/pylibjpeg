499cf662f47eb9268c377ff70fdf3cccf9fa9628
scaramallion <scaramallion@users.noreply.github.com>
2020-02-26 11:46:23 +1100

    Try with direct install

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -14,9 +14,7 @@ echo "Working directory: " $PWD
 echo ""
 
 if [[ "$TEST_SUITE" == "pydicom_master" ]]; then
-    wget https://github.com/pydicom/pydicom/archive/master.tar.gz -O /tmp/pydicom.tar.gz
-    tar xzf /tmp/pydicom.tar.gz
-    pip install $PWD/pydicom-master
+    pip install git+https://github.com/pydicom/pydicom.git
     python -c "import pydicom; print('pydicom version', pydicom.__version__)"
 elif [[ "$TEST_SUITE" == "pydicom_release" ]]; then
     pip install pydicom
